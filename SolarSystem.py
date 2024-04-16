af25203c8ff08131672d8f47a0a5e41e31be4590
storresp <santiagotopo@unisabana.edu.co>
2024-04-16 11:01:37 -0500

    Hola chicos segundo commit, defini la clase y tierra y sol y probe la fuerza y si sirve letsgo :)

--- SolarSystem.py
+++ SolarSystem.py
@@ -2,24 +2,11 @@ import math
 import numpy as np
 
 class Planeta:
-  def __init__(self, peso, posX, posY, posZ, VoX, VoY, VoZ):
-    self.peso = peso*1000
-    self.posX = posX*1000
-    self.posY = posY*1000
-    self.posZ = posZ*1000
-    self.VoX = VoX*1000
-    self.VoY = VoY*1000
-    self.VoZ = VoZ*1000
 
-Mercurio = Planeta()
-Venus = Planeta()
-Tierra = Planeta()
-Marte = Planeta()
-Saturno = Planeta()
-Urano = Planeta()
-Jupiter = Planeta()
-Neptuno = Planeta()
-
+  def __init__(self, masa, posicion, velocidad):
+    self.masa = masa*1000
+    self.posicion = posicion*1000
+    self.velocidad = velocidad*1000 #Se multiplica por mil para obtener el valor en metros y m/s respectivamente.
 
 def fuerza(m1, m2, r1, r2):
 
@@ -40,19 +27,20 @@ def fuerza(m1, m2, r1, r2):
     d = math.sqrt(r2[0]**2 + r2[1]**2 + r2[2]**2) #distancia entre los dos objetos [m]
 
     run21 = r2 / d #vector unitario que apunta de 1 hacia 2
-
     
     #calcular la fuerza
 
     F = run21 * G * m1 * m2 / d**2
     return F
 
-#definir las masas y posiciones de los objetos
-m1 = 54564654654e-10 #masa del objeto 1 [kg]
-r1 = [141654646116165, 51561654654121654654, 5365464651654654651] #posición del objeto 1 [m]
-m2 = 54564654654e-10 #masa del objeto 2 [kg]
-r2 = [546546546e-18, 546546546546e10, 5465465e-4] #posición del objeto 2 [m]
+Sol = Planeta(1.989E+30, 
+              [-1.179443251112733E+06, -4.525307891739777E+05, 3.134211075848844E+04], 
+              [8.641673046632211E-03, -1.197549843610353E-02, -8.333309545496825E-05])
+
+Tierra = Planeta(5.972E+24, 
+                 [-6.807583303581282E+07, 1.306386634514613E+08, 2.441434724599123E+04], 
+                 [-2.700500662780606E+01, -1.367492208542078E+01, -2.819099962243499E-04])
 
 #calcular la fuerza
-F = fuerza(m1, m2, r1, r2)
+F = fuerza(Sol.masa, Tierra.masa, Sol.posicion, Tierra.posicion)
 print(F)
